81b5f830c3be51813e192d7a8c49a8012da7c66b
crosshj <github@chimpjuice.com>
2022-09-04 00:56:18 -0400

    selecting and remembering tool

--- left/left.js
+++ left/left.js
@@ -3,14 +3,27 @@
 
 import { send } from '../shared/messages.js';
 
+const selectTool = (tool) => {
+	const newTool = document.body.querySelector(`.icon.${tool}`);
+	if(!newTool) return console.error(`Could not find tool: ${tool}`);
+
+	const currentSelected = document.body.querySelector('.selected');
+	if(currentSelected === newTool) return;
+
+	currentSelected?.classList.remove('selected');
+	newTool.classList.add('selected');
+	send('tool-select', { tool });
+	localStorage.setItem('gfx-currentTool', tool);
+};
+
 document.addEventListener('pointerdown', (e) => {
 	if(!e.target.classList.contains('icon')) return;
-	const currentSelected = document.body.querySelector('.selected');
-	if(currentSelected === e.target) return;
-	currentSelected?.classList.remove('selected');
-	e.target.classList.add('selected');
 	const tool = e.target.classList.toString().split(" ")
 		.filter(x => !["selected", "icon"].includes(x))
 		.join(" ");
-	send('tool-select', { tool });
+	selectTool(tool);
 });
+
+setTimeout(() => {
+	selectTool(localStorage.getItem('gfx-currentTool') || "pencil");
+}, 100);
