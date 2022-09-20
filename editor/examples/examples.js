4c31ede5b553c3cf05d3a0f54491bdd0d42f4f13
crosshj <github@chimpjuice.com>
2022-09-20 19:57:26 -0400

    shaders WIP

--- editor/examples/examples.js
+++ editor/examples/examples.js
@@ -90,19 +90,62 @@ const webgl = `
 // example modded from:
 // https://www.tutorialspoint.com/webgl/webgl_sample_application.htm
 
-// shaders
+var gl = ctx;
+
 var vertCode = \`${vertShader()}\`;
 
 var fragCode = \`${fragShader()}\`;
+
+var vertices = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5,];
+var vertex_buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+//var vertCode = \`
+//	attribute vec2 coordinates; 
+//	void main(void) {
+//		gl_Position = vec4(coordinates,0.0, 1.0);
+//	}
+//\`;
+var vertShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertShader, vertCode);
+gl.compileShader(vertShader);
+
+//var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' + '}';
+var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragShader, fragCode);
+gl.compileShader(fragShader);
+
+var shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram, vertShader); 
+gl.attachShader(shaderProgram, fragShader);
+gl.linkProgram(shaderProgram);
+gl.useProgram(shaderProgram);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+
+gl.enableVertexAttribArray(coord);
+gl.clearColor(0.5, 0.5, 0.5, 0.9);
+
+gl.enable(gl.DEPTH_TEST); 
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.viewport(0,0, width, height);
+gl.drawArrays(gl.TRIANGLES, 0, 3);
 `;
+
 	//.concat(document.getElementById("gl-function").text);
 
 export default () => ({
-	webgl,
-	triangle,
-	square,
-	octagon,
 	circle,
+	octagon,
 	smiley,
+	square,
 	text,
+	triangle,
+	webgl,
 });
