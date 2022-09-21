b0f33586ed3b80dfebc90cac76dfe12b19a72cb1
crosshj <github@chimpjuice.com>
2022-09-20 20:01:07 -0400

    shaders WIP

--- editor/examples/examples.js
+++ editor/examples/examples.js
@@ -92,27 +92,27 @@ const webgl = `
 
 var gl = ctx;
 
-var vertCode = \`${vertShader()}\`;
-
-var fragCode = \`${fragShader()}\`;
-
 var vertices = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5,];
 var vertex_buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-//var vertCode = \`
-//	attribute vec2 coordinates; 
-//	void main(void) {
-//		gl_Position = vec4(coordinates,0.0, 1.0);
-//	}
-//\`;
+var vertCode = \`
+	attribute vec2 coordinates; 
+	void main(void) {
+		gl_Position = vec4(coordinates,0.0, 1.0);
+	}
+\`;
 var vertShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertShader, vertCode);
 gl.compileShader(vertShader);
 
-//var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' + '}';
+var fragCode = \`
+	void main(void) {
+		gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);
+	}
+\`;
 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragShader, fragCode);
 gl.compileShader(fragShader);
@@ -136,8 +136,14 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 
 gl.viewport(0,0, width, height);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
-`;
 
+
+/*
+var vertCode = \`${vertShader()}\`;
+
+var fragCode = \`${fragShader()}\`;
+*/
+`;
 	//.concat(document.getElementById("gl-function").text);
 
 export default () => ({
