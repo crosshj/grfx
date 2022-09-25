5d81c3f78c71112b6183abf1d8204b5697eb2806
crosshj <github@chimpjuice.com>
2022-09-24 23:31:41 -0400

    v0.144.0 of threejs

--- importmap.js
+++ importmap.js
@@ -4,8 +4,8 @@ const imports = {
 	"immer": "https://cdn.skypack.dev/-/immer@v7.0.15-7TvRXFAjwhTKJWP7XBf6/dist=es2019,mode=imports/optimized/immer.js",
 	"lodash/get": "https://cdn.skypack.dev/-/lodash.get@v4.4.2-BmwyhyYUfa0zoZ0JKXri/dist=es2019,mode=imports/optimized/lodash.get.js",
 	"redux": "https://cdn.skypack.dev/-/redux@v4.1.1-XG1G4MAJLJXXzlRpXjOG/dist=es2019,mode=imports/optimized/redux.js",
-	"three": "https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.module.js",
-	"three-objloader": "https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/OBJLoader.js",
+	"three": "https://cdn.jsdelivr.net/npm/three@0.144.0/build/three.module.js",
+	"three-objloader": "https://cdn.jsdelivr.net/npm/three@0.144.0/examples/jsm/loaders/OBJLoader.js",
 	"three-subdivide": "https://unpkg.com/three-subdivide@1.0.2/build/index.module.js"
 };
 
