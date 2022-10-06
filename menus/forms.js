d79c87175d0332ad62991292e6004f5025f2c079
crossar <41621244+crossar@users.noreply.github.com>
2022-10-06 19:46:48 -0400

    Update forms.js

--- menus/forms.js
+++ menus/forms.js
@@ -4,10 +4,6 @@ export default {
 	fileOpen: new URL(import.meta.url).href + '/../forms/fileOpen.hbs',
 	fileSaveAs: new URL(import.meta.url).href + '/../forms/fileSaveAs.hbs',
 	filter: new URL(import.meta.url).href + '/../forms/filter.hbs',
-	filterBlur: new URL(import.meta.url).href + '/../forms/filterBlur.hbs',
-	filterNoise: new URL(import.meta.url).href + '/../forms/filterNoise.hbs',
-	filterPixelate: new URL(import.meta.url).href + '/../forms/filterPixelate.hbs',
-	filterSharpen: new URL(import.meta.url).href + '/../forms/filterSharpen.hbs',
 	imageSize: new URL(import.meta.url).href + '/../forms/imageSize.hbs',
 	layerNew: new URL(import.meta.url).href + '/../forms/layerNew.hbs',
 };
