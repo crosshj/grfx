b2a5b72ec2695d0f4410f54de89d02f63117a2d3
crosshj <github@chimpjuice.com>
2022-11-17 11:25:46 -0500

    refactor state test

--- core/state.test.js
+++ core/state.test.js
@@ -3,30 +3,25 @@ import { HostState, ClientState } from './state.js';
 
 const logJSON = x => console.log(JSON.stringify(x, null, 2));
 
+const LAYER_ONE_ID = "867f51d2-5b72-479d-83a8-602b42059075";
+const LAYER_TWO_ID = "70b129f8-1e7d-4080-854c-1279ea63dd86";
+
 const state = {
 	editor: {
 		zoom: 0,
-		tool: {
-			id: "airbrush"
-		},
+		tool: { id: "airbrush" },
 	},
 	file: {
-		canvas: {
-			width: 0,
-			height: 0,
-		},
+		canvas: { width: 0, height: 0},
 		history: [],
 		layerOrder: [
-			"867f51d2-5b72-479d-83a8-602b42059075",
-			"70b129f8-1e7d-4080-854c-1279ea63dd86"
+			LAYER_ONE_ID,
+			LAYER_TWO_ID
 		],
-		layers: [{
-			name: "old",
-			id: "867f51d2-5b72-479d-83a8-602b42059075"
-		}, {
-			name: "new",
-			id: "70b129f8-1e7d-4080-854c-1279ea63dd86"
-		}]
+		layers: [
+			{ name: "old", id: LAYER_ONE_ID },
+			{ name: "new", id: LAYER_TWO_ID },
+		]
 	}
 };
 
@@ -62,34 +57,34 @@ describe('Host State', () => {
 	it('should keep history', () => {
 		const changes = [];
 		const s = new HostState(state, (state, { patch }) => changes.push(patch));
+
 		s.layer.order();
-		const historyChange = changes[1];
+
+		const { op, path } = changes[1];
 		expect(changes.length).toEqual(2);
-		expect(historyChange.op).toEqual("add");
-		expect(historyChange.path).toEqual("/file/history/0");
+		expect(op).toEqual("add");
+		expect(path).toEqual("/file/history/0");
 	});
 
 	it('should track all changes', () => {
 		const changes = [];
-		const s = new HostState(state, (state, change) => {
-			changes.push({ state, change });
-		});
+		const s = new HostState(state, (state, { patch }) => changes.push(patch));
 
 		s.layer.order([
-			"70b129f8-1e7d-4080-854c-1279ea63dd86",
-			"867f51d2-5b72-479d-83a8-602b42059075",
+			LAYER_TWO_ID,
+			LAYER_ONE_ID,
 		]);
 		s.layer.add({ name: "newer" })
 		s.undo();
 		s.redo();
-		s.layer.remove("867f51d2-5b72-479d-83a8-602b42059075");
+		s.layer.remove(LAYER_ONE_ID);
 		s.undo();
-		s.layer.update("867f51d2-5b72-479d-83a8-602b42059075", { opacity: 0.5 });
-		s.layer.update("867f51d2-5b72-479d-83a8-602b42059075", { opacity: 1, selected: false });
+		s.layer.update(LAYER_ONE_ID, { opacity: 0.5 });
+		s.layer.update(LAYER_ONE_ID, { opacity: 1, selected: false });
 
-		const exampleChange = changes[0].change.patch;
-		expect(exampleChange.op).toEqual("replace");
-		expect(exampleChange.path).toEqual("/file/layerOrder");
+		const { op, path } = changes[0];
+		expect(op).toEqual("replace");
+		expect(path).toEqual("/file/layerOrder");
 		expect(changes.length).toEqual(32);
 	});
 });
@@ -98,6 +93,7 @@ describe('Host State', () => {
 describe('Client State', () => {
 	it('should reconstitute state from patches', () => {
 		const s = ClientState(state, patches);
+
 		const { history, layers } = s.file;
 		expect(layers[0].opacity).toEqual(1);
 		expect(layers[0].selected).toEqual(false);
