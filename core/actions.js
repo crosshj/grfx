90b9c97650ab014493f35397d25e9d594bf39f64
crossar <41621244+crossar@users.noreply.github.com>
2022-11-21 15:59:10 -0500

    Update actions.js

--- core/actions.js
+++ core/actions.js
@@ -537,6 +537,9 @@ const filterMain = (which) => async (context) => {
 			binarizeAmount, 
 			edgeAmount, 
 			ditherAmount,
+			rescaleAmount,
+			pixelateAmount,
+			sharpenAmount,
 			
 			/* etc */ } = form;
 		
@@ -564,6 +567,18 @@ const filterMain = (which) => async (context) => {
 		if(which === "blur") {
 			l.def = l.def.replace(/\nops.filter\("StackBlur",.*\);/g, '');
 			l.def += '\n' + `ops.filter("StackBlur", ${blurAmount});`;
+		}
+		if(which === "rescale") {
+			l.def = l.def.replace(/\nops.filter\("Rescale",.*\);/g, '');
+			l.def += '\n' + `ops.filter("Rescale", ${rescaleAmount});`;
+		}
+		if(which === "pixelate") {
+			l.def = l.def.replace(/\nops.filter\("Mosaic",.*\);/g, '');
+			l.def += '\n' + `ops.filter("Mosaic", ${pixelateAmount});`;
+		}
+		if(which === "sharpen") {
+			l.def = l.def.replace(/\nops.filter\("Sharpen",.*\);/g, '');
+			l.def += '\n' + `ops.filter("Sharpen", ${sharpenAmount});`;
 		}
 
 		// -------
